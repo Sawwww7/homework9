ec158ebf63a3f65fc2b594eaef843b22ce0b4399
Andrii Fedorov <sawwww7@gmail.com>
2024-06-06 00:58:43 +0300

    homework9

---
+++ index.js
@@ -0,0 +1,53 @@
+const primeNumber = (arr) => arr.filter(number);
+const number = (cur) => {
+  for (let x = 2; x < cur; x++) {
+    if (cur % x == 0) {
+      return false;
+    }
+  }
+  return cur > 1;
+};
+
+const array1 = [1, 2, 3, 4, 5, 5, 3, 3, 5, 20, 37, 41, 83, 97, 255];
+console.log(primeNumber(array1)); //[2, 3, 5, 5, 3, 3, 5, 37, 41, 83, 97]
+////////////////////////////////////////////////////////////////////
+const recreateArr = (arr) => {
+  const message = Object.fromEntries(arr.map((cur) => [cur.source, cur]));
+  return message;
+};
+const students = [
+  { source: "Telephone", text: "Hello", date: "05.06.2024" },
+  { source: "Computer", text: "How are you", date: "06.06.2024" },
+  { source: "Server", text: "Attention", date: "07.06.2024" },
+];
+console.log(recreateArr(students));
+/////////////////////////////////////////
+const groupProduct = (arr) => {
+  const groupByCategory = arr.reduce((group, product) => {
+    const {type} = product;
+    group[type] = group[type] ?? [];
+    group[type].push(product);
+    return group;
+  }, {});
+  return groupByCategory;
+};
+const data = [
+  { type: "food", name: "Pizza" },
+  { type: "drink", name: "Coffee" },
+  { type: "food", name: "Hot Dog" },
+];
+console.log(groupProduct(data)); //drink: {type: 'drink', name: 'Coffee'}
+                                 //food:  {type: 'food', name: 'Pizza'}
+                                 //       {type: 'food', name: 'Hot Dog'}
+
+
+const groupProduct2 = Object.groupBy(data, (item) => item.type);
+console.log(groupProduct2);       //drink: {type: 'drink', name: 'Coffee'}
+                                  //food:  {type: 'food', name: 'Pizza'}
+                                  //       {type: 'food', name: 'Hot Dog'}
+
+
+//Вам необхідно написати функцію, яка приймає на вхід масив чисел і повертає новий масив, що містить тільки ті числа, які є простими числами.
+//Вам необхідно написати функцію, яка приймає на вхід масив об'єктів, де кожен об'єкт описує сповіщення та має поля source / text / date.
+//Вам необхідно перетворити цей масив на об'єкт, де ключем буде джерело сповіщення, а значенням - масив сповіщень із цього джерела.
+//Вам необхідно написати функцію, яка приймає на вхід масив і повністю повторює поведінку методу масиву group
